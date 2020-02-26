936c6b46641373a25138f9523a92c44fe276d111
gengotsukai <benioni2005@yahoo.co.jp>
2020-02-26 18:40:28 +0900

    finished chapter 8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7b757231c7a348f20fcd74f5be1b3347c0d846b73c08848d618232e6991a6a3fc8ac1f47150dfdbf02a42856747fda0354a46fd303b1c53578a69156f7ecc1c2'
