7afb148ca2ff9083bb1b54dd796cc8ebe975d32f
gengotsukai <benioni2005@yahoo.co.jp>
2020-02-26 16:35:09 +0900

    chapter 6 finished

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -6,15 +6,17 @@ class TodolistsController < ApplicationController
 
   def create
         # ストロングパラメーターを使用
-         list = List.new(list_params)
-        # DBへ保存する
-         list.save
-        # トップ画面へリダイレクト
-        redirect_to '/top'
+        list = List.new(list_params)
+        list.save
+        redirect_to todolist_path(list.id)
     end
 
     def index
         @lists = List.all
+    end
+
+    def show
+      @list = List.find(params[:id])
     end
 
     private
